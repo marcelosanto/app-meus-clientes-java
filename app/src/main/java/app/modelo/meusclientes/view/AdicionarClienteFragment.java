8699aeffbb5e37217ae8f43517420ea9036ef84e
Marcelo Santos <rocha.mer21@gmail.com>
2021-08-20 23:31:32 -0300

    feito o layout da tela add cliente, e criado os importe dos editTexts

--- app/src/main/java/app/modelo/meusclientes/view/AdicionarClienteFragment.java
+++ app/src/main/java/app/modelo/meusclientes/view/AdicionarClienteFragment.java
@@ -1,21 +1,32 @@
 package app.modelo.meusclientes.view;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import app.modelo.meusclientes.R;
+import app.modelo.meusclientes.controller.ClienteController;
+import app.modelo.meusclientes.model.Cliente;
 
 
 public class AdicionarClienteFragment extends Fragment {
 
+    //Declaração global de atributos - variaveis globais
     View view;
+    TextView txtTitulo;
+    EditText editNome, editTelefone, editEmail, editCep, editLogradouro, editNumero, editBairro, editCidade, editEstado;
+    CheckBox chkTermosdeUso;
+    Button btnCancelar, btnSalvar;
+
+    Cliente novoCliente;
+    ClienteController clienteController;
 
     public AdicionarClienteFragment() {
     }
@@ -33,13 +44,57 @@ public class AdicionarClienteFragment extends Fragment {
 
         view =  inflater.inflate(R.layout.fragment_adicionar_cliente, container, false);
 
-        TextView txtTitulo = view.findViewById(R.id.txtTitulo);
+        iniciarComponentesDeLayout();
 
-        txtTitulo.setText(R.string.fragmento_adicionar_cliente);
-
-        txtTitulo.setTextColor(ColorStateList.valueOf(Color.BLACK));
+        escutarEventosDosBotoes();
 
         return view;
+    }
+
+
+
+    /**
+     * Inicializar os componentes da tela
+     * para adicionar os clientes
+     * */
+    private void iniciarComponentesDeLayout() {
+        txtTitulo = view.findViewById(R.id.txtTitulo);
+        txtTitulo.setText(R.string.novo_cliente);
+
+        editNome = view.findViewById(R.id.editNomeCompleto);
+        editTelefone = view.findViewById(R.id.editTelefone);
+        editEmail = view.findViewById(R.id.editEmail);
+        editLogradouro = view.findViewById(R.id.editLogradouro);
+        editNumero = view.findViewById(R.id.editNumero);
+        editCep = view.findViewById(R.id.editCep);
+        editBairro = view.findViewById(R.id.editBairro);
+        editCidade = view.findViewById(R.id.editCidade);
+        editEstado = view.findViewById(R.id.editEstado);
+
+        chkTermosdeUso = view.findViewById(R.id.chkTermosDeUso);
+
+        btnCancelar = view.findViewById(R.id.btnCancelar);
+        btnSalvar = view.findViewById(R.id.btnSalvar);
+
+        novoCliente = new Cliente();
+        clienteController = new ClienteController(getContext());
+
+    }
+
+    private void escutarEventosDosBotoes() {
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clienteController.incluir(novoCliente);
+            }
+        });
     }
 
 
